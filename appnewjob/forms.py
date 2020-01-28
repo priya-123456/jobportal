8c75f1305c2f394c241d916e361177fee16d76d7
sreevani-github <sreevani@mhninformatics.com>
2020-01-28 11:42:55 +0530

    shakeela

--- appnewjob/forms.py
+++ appnewjob/forms.py
@@ -1,11 +1,14 @@
+
 from django import forms
 from .models import Resume, Createjob
+
 
 
 class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         fields = "__all__"
+
 
 
 class JobForm(forms.ModelForm):
